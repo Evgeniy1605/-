091a0ac5510fee71c633c8a949138247fd710ea5
pikh1 <pikh1@LAPTOP-2LAUS2MF>
2022-10-06 12:23:42 +0300

    Add project files.

---
+++ ТЗ/Controllers/DirectoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using ТЗ.Models;
+
+namespace ТЗ.Controllers
+{
+    public class DirectoryController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public DirectoryController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+         
+        public IActionResult Index()
+        {
+            var path = Path.Combine(_environment.WebRootPath, "Directory");
+            string[] files = Directory.GetDirectories(path);
+            DirectoryModel model = new DirectoryModel();
+            model.ArrFiles = files;
+            model.Name = Path.GetFileName(path);
+            return View(model);
+        }
+
+        public IActionResult ReflactFile(string name)
+        {
+            string[] files = Directory.GetDirectories(name);
+            DirectoryModel model = new DirectoryModel();
+            model.ArrFiles = files;
+            model.Name = Path.GetFileName(name);
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult GetRepositiry( List<IFileInfo> File)
+        {
+
+            
+            return View("Index");
+        }
+    }
+}
---
+++ ТЗ/Models/DirectoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ТЗ.Models
+{
+    public class DirectoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[]? ArrFiles { get; set; }
+
+    }
+}
